74f1bf376526578e0ee9e1a9bbec609895a89661
latif2002 <lotfee2002@gmail.com>
2015-02-23 02:03:26 -0500

    fix add my class

--- src/LatifObject.java
+++ src/LatifObject.java
@@ -1,4 +1,39 @@
+import java.util.*;
 
-public class LatifObject {
+public class LatifObject implements Iterator
+{
+	LatifObject(int num){
+		generate(num);
+	}
+	
+	
+	
+	private void generate(int num) {
+		// TODO Auto-generated method stub
 
+			
+		
+			
+			
+	}
+
+
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Integer next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+	
+	
+	
 }
